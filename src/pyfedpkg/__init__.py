2e2e638ba8b28968b8f46a446fa81f175efc97fc
Jesse Keating <jkeating@redhat.com>
2011-05-22 15:41:50 -0700

    Move the fixbranches.py script out of the $PATH (#698646)

--- src/pyfedpkg/__init__.py
+++ src/pyfedpkg/__init__.py
@@ -106,7 +106,7 @@ def _check_newstyle_branches(path=None, repo=None):
             re.match(refsre, ref.name):
                 log.error('This repo has old style branches but upstream '
                           'has converted to new style.  Please run '
-                          'fedpkg-fixbranches.py to fix your repo')
+                          '/usr/libexec/fedpkg-fixbranches.py to fix your repo')
                 raise FedpkgError('Unconverted branches')
 
 def _find_branch(path=None, repo=None):
