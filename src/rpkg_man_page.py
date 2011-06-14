e71aafc9bf318e657341b9ac9902cb5e002dfca6
Jesse Keating <jkeating@redhat.com>
2011-06-14 15:29:06 -0700

    Add a missing license statement

--- src/rpkg_man_page.py
+++ src/rpkg_man_page.py
@@ -1,4 +1,14 @@
 # Print a man page from the help texts.
+#
+# Copyright (C) 2011 Red Hat Inc.
+# Author(s): Jesse Keating <jkeating@redhat.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+# the full text of the license.
+
 
 
 import sys
