4e835a3b1db18f14414d71ad25d1ba50f270816d
Jesse Keating <jkeating@redhat.com>
2011-06-14 15:37:19 -0700

    Add a setup.py file and MANFEST.in
    These help us build and "install" the software

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+from setuptools import setup, command
+
+setup(
+    name = "rpkg",
+    version = "1.0",
+    author = "Jesse Keating",
+    author_email = "jkeating@redhat.com",
+    description = ("A python library and runtime script for managing RPM"
+                                   "package sources in a git repository"),
+    license = "GPLv2+",
+    url = "https://fedorahosted.org/rpkg",
+    package_dir = {'': 'src'},
+    packages = ['pyrpkg'],
+    scripts = ['src/rpkg'],
+    data_files = [('/etc/bash_completion.d', ['src/rpkg.bash']),
+                  ('/etc/rpkg', ['src/rpkg.conf'])],
+)
