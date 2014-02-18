ccbc2a0c09ed9a97b288b69aa6fac96791eafaa9
Dennis Gilmore <dennis@ausil.us>
2014-02-18 08:10:41 -0600

    add missing ,

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, command
 setup(
     name = "rpkg",
     version = "1.20",
-    author = "Dennis Gilmore"
+    author = "Dennis Gilmore",
     author_email = "ausil@fedoraproject.org",
     description = ("A python library and runtime script for managing RPM"
                                    "package sources in a git repository"),
