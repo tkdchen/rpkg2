2240ff806ebf1a76eadb142693aa1c4306307708
Pavol Babincak <pbabinca@redhat.com>
2014-10-09 15:46:31 +0200

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -40,4 +40,17 @@ setup(
     data_files = [('/etc/bash_completion.d', ['src/rpkg.bash']),
                   ('/etc/rpkg', ['src/rpkg.conf'])],
     cmdclass={'test': DiscoverTest},
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
 )
