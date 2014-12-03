00ada7f9880d8eed6d56fc1b0356128b1110ad93
Pavol Babincak <pbabinca@redhat.com>
2014-12-03 17:52:46 +0100

    1.29 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="rpkg",
-    version="1.28",
+    version="1.29",
     author="Dennis Gilmore",
     author_email="ausil@fedoraproject.org",
     description=("A python library and runtime script for managing RPM"
