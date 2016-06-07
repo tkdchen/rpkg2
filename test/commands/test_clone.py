094303a93c10890a6db1d2969e49a9c74924ef35
Lubomír Sedlář <lsedlar@redhat.com>
2016-06-07 17:03:10 +0200

    Add tests for cloning with a namespace
    Signed-off-by: Lubomír Sedlář <lsedlar@redhat.com>

--- test/commands/test_clone.py
+++ test/commands/test_clone.py
@@ -32,6 +32,26 @@ class CommandCloneTestCase(CommandTestCase):
         self.assertEqual(confgit.config('sendemail.to'),
                          "%s-owner@fedoraproject.org" % self.module)
 
+    def test_clone_anonymous_with_namespace(self):
+        self.module = 'rpms/module1'
+        self.make_new_git(self.module)
+
+        import pyrpkg
+        cmd = pyrpkg.Commands(self.path, self.lookaside, self.lookasidehash,
+                              self.lookaside_cgi, self.gitbaseurl,
+                              self.anongiturl, self.branchre, self.kojiconfig,
+                              self.build_client, self.user, self.dist,
+                              self.target, self.quiet, distgit_namespaced=True)
+        cmd.clone_config = CLONE_CONFIG
+        cmd.clone(self.module, anon=True)
+
+        moduledir = os.path.join(self.path, 'module1')
+        self.assertTrue(os.path.isdir(os.path.join(moduledir, '.git')))
+        confgit = git.Git(moduledir)
+        self.assertEqual(confgit.config('bz.default-component'), self.module)
+        self.assertEqual(confgit.config('sendemail.to'),
+                         "%s-owner@fedoraproject.org" % self.module)
+
     def test_clone_anonymous_with_path(self):
         self.make_new_git(self.module)
 
@@ -110,6 +130,24 @@ class CommandCloneTestCase(CommandTestCase):
                               self.anongiturl, self.branchre, self.kojiconfig,
                               self.build_client, self.user, self.dist,
                               self.target, self.quiet)
+        cmd.clone(
+            self.module, anon=True, branch='rpkg-tests-1', target='new_clone')
+
+        with open(os.path.join(
+                self.path, 'new_clone', '.git', 'HEAD')) as HEAD:
+            self.assertEqual(HEAD.read(), 'ref: refs/heads/rpkg-tests-1\n')
+
+    def test_clone_into_dir_with_namespace(self):
+        self.module = 'rpms/module1'
+        self.make_new_git(self.module,
+                          branches=['rpkg-tests-1', 'rpkg-tests-2'])
+
+        import pyrpkg
+        cmd = pyrpkg.Commands(self.path, self.lookaside, self.lookasidehash,
+                              self.lookaside_cgi, self.gitbaseurl,
+                              self.anongiturl, self.branchre, self.kojiconfig,
+                              self.build_client, self.user, self.dist,
+                              self.target, self.quiet, distgit_namespaced=True)
         cmd.clone(
             self.module, anon=True, branch='rpkg-tests-1', target='new_clone')
 
