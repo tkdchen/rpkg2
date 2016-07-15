531a90edace3b6431ceb2f986af6842ec4e436be
Chenxiong Qi <cqi@redhat.com>
2016-07-15 17:29:59 +0800

    1.46 Release
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="rpkg",
-    version="1.45",
+    version="1.46",
     author="Dennis Gilmore",
     author_email="ausil@fedoraproject.org",
     description=("A python library and runtime script for managing RPM"
