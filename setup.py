59ba6a7f8fc45fc9764aab358ff655985e5b45be
Chenxiong Qi <cqi@redhat.com>
2017-12-17 22:16:53 +0800

    Set install and tests requires in setup.py
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python
 
+import os
+
 from setuptools import setup, find_packages
 
+
+def read_requirements(requirements_file):
+    specifiers = []
+    dep_links = []
+
+    def read_lines():
+        with open(requirements_file, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line == '' or line.startswith('#') or line.startswith('-r'):
+                    continue
+                yield line
+
+    for line in read_lines():
+        if line.startswith('git+'):
+            dep_links.append(line)
+        else:
+            specifiers.append(line)
+
+    return specifiers, dep_links
+
+setup_py_path = os.path.dirname(os.path.realpath(__file__))
+pypi_txt = os.path.join(setup_py_path, 'requirements', 'pypi.txt')
+test_pypi_txt = os.path.join(setup_py_path, 'requirements', 'test-pypi.txt')
+
+install_requires, dep_links = read_requirements(pypi_txt)
+tests_require, test_dep_links = read_requirements(test_pypi_txt)
+dep_links += test_dep_links
 
 setup(
     name="rpkg",
@@ -13,8 +43,9 @@ setup(
     license="GPLv2+",
     url="https://pagure.io/rpkg",
     packages=find_packages(),
-    install_requires=['six', 'pycurl', 'cccolutils'],  # + koji, but it's not in PyPI
-    tests_require=['nose', 'mock', 'GitPython'],
+    install_requires=install_requires,
+    tests_require=tests_require,
+    dependency_links=dep_links,
     test_suite='nose.collector',
     classifiers=(
         'Development Status :: 5 - Production/Stable',
