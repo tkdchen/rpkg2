e2fea4a57f74acdb2a26233cec088c2e58ecca57
Chenxiong Qi <cqi@redhat.com>
2017-12-28 13:21:45 +0800

    Declare Python versions rpkg can work with
    Fix #278
    
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ),
